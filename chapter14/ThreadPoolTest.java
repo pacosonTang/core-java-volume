f21670630f2490db82cea38de482fabad7facfc5
pacosonTang <pacoson@163.com>
2015-12-20 09:46:12 +0800

    test for thread pool

---
+++ chapter14/ThreadPoolTest.java
@@ -0,0 +1,130 @@
+package com.corejava.chapter14_4;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * @version 1.01 2012-01-26
+ * @author Cay Horstmann
+ */
+public class ThreadPoolTest
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter base directory (e.g. /usr/local/jdk5.0/src): ");
+      String directory = in.nextLine();
+      System.out.print("Enter keyword (e.g. volatile): ");
+      String keyword = in.nextLine();
+
+      ExecutorService pool = Executors.newCachedThreadPool();
+
+      MatchCounterCopy counter = new MatchCounterCopy(new File(directory), keyword, pool);
+      System.out.println("before Future<Integer> result = pool.submit(counter);");
+      Future<Integer> result = pool.submit(counter);
+
+      try
+      {
+         System.out.println(result.get() + " matching files.");
+      }
+      catch (ExecutionException e)
+      {
+         e.printStackTrace();
+      }
+      catch (InterruptedException e)
+      {
+      }
+      pool.shutdown();
+
+      int largestPoolSize = ((ThreadPoolExecutor) pool).getLargestPoolSize();
+      System.out.println("largest pool size=" + largestPoolSize);
+   }
+}
+
+/**
+ * This task counts the files in a directory and its subdirectories that contain a given keyword.
+ */
+class MatchCounterCopy implements Callable<Integer>
+{
+   private File directory;
+   private String keyword;
+   private ExecutorService pool;
+   private int count;
+
+   /**
+    * Constructs a MatchCounter.
+    * @param directory the directory in which to start the search
+    * @param keyword the keyword to look for
+    * @param pool the thread pool for submitting subtasks
+    */
+   public MatchCounterCopy(File directory, String keyword, ExecutorService pool)
+   {
+      this.directory = directory;
+      this.keyword = keyword;
+      this.pool = pool;
+   }
+
+   public Integer call()
+   {
+	   System.out.println("calling func call");
+      count = 0;
+      try
+      {
+         File[] files = directory.listFiles();
+         List<Future<Integer>> results = new ArrayList<>();
+
+         for (File file : files)
+            if (file.isDirectory())
+            {
+            	MatchCounterCopy counter = new MatchCounterCopy(file, keyword, pool);
+               Future<Integer> result = pool.submit(counter);
+               results.add(result);
+            }
+            else
+            {
+               if (search(file)) count++;
+            }
+
+         for (Future<Integer> result : results)
+            try
+            {
+               count += result.get();
+            }
+            catch (ExecutionException e)
+            {
+               e.printStackTrace();
+            }
+      }
+      catch (InterruptedException e)
+      {
+      }
+      return count;
+   }
+
+   /**
+    * Searches a file for a given keyword.
+    * @param file the file to search
+    * @return true if the keyword is contained in the file
+    */
+   public boolean search(File file)
+   {
+      try
+      {
+         try (Scanner in = new Scanner(file))
+         {
+            boolean found = false;
+            while (!found && in.hasNextLine())
+            {
+               String line = in.nextLine();
+               if (line.contains(keyword)) found = true;
+            }         
+            return found;
+         }
+      }
+      catch (IOException e)
+      {
+         return false;
+      }
+   }
+}
