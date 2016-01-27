76f9be7b0f0150c59de66285fdea1c4d082a28fb
pacosonTang <pacoson@163.com>
2016-01-27 17:27:28 +0800

    implementation for building server accepting multiple clients link request

---
+++ coreJavaAdvanced/chapter3/ThreadedEchoServer.java
@@ -0,0 +1,83 @@
+package com.corejava.chapter3;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ThreadedEchoServer
+{  
+   public static void main(String[] args )
+   {  
+      try
+      {  
+         int i = 1;
+         ServerSocket s = new ServerSocket(8189);
+
+         while (true)
+         {  
+            Socket incoming = s.accept();
+            System.out.println("successful link with thread  " + i);
+            Runnable r = new ThreadedEchoHandler(incoming);
+            Thread t = new Thread(r);
+            t.start();
+            i++;
+         }
+      }
+      catch (IOException e)
+      {  
+         e.printStackTrace();
+      }
+   }
+}
+
+/**
+   This class handles the client input for one server socket connection. 
+*/
+class ThreadedEchoHandler implements Runnable
+{ 
+   private Socket incoming;
+
+   /**
+      Constructs a handler.
+      @param i the incoming socket
+   */
+   public ThreadedEchoHandler(Socket i)
+   { 
+      incoming = i; 
+   }
+
+   public void run()
+   {  
+      try
+      {  
+         try
+         {
+            InputStream inStream = incoming.getInputStream();
+            OutputStream outStream = incoming.getOutputStream();
+            
+            Scanner in = new Scanner(inStream);         
+            PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+            
+            out.println( "Hello! Enter BYE to exit." );
+            
+            // echo client input
+            boolean done = false;
+            while (!done && in.hasNextLine())
+            {  
+               String line = in.nextLine();            
+               out.println("Echo: " + line);            
+               if (line.trim().equals("BYE"))
+                  done = true;
+            }
+         }
+         finally
+         {
+            incoming.close();
+         }
+      }
+      catch (IOException e)
+      {  
+         e.printStackTrace();
+      }
+   }
+}
