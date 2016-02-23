ca3dfcf517d9527c11818722266ae9ba3e10b43c
pacosonTang <pacoson@163.com>
2016-02-23 13:19:46 +0800

    test for source code annotation

---
+++ coreJavaAdvanced/chapter10/10_6/Property.java
@@ -0,0 +1,10 @@
+package com.corejava.chapter10_6;
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Property
+{
+   String editor() default ""; 
+}
